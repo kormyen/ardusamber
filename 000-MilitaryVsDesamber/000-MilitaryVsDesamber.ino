00044a8cf7af40379a0b83b049c53f120557b292
kor <h@kor.co.nz>
2018-05-17 00:03:11 +1200

    Refactor.

--- 000-MilitaryVsDesamber/000-MilitaryVsDesamber.ino
+++ 000-MilitaryVsDesamber/000-MilitaryVsDesamber.ino
@@ -4,8 +4,6 @@ It includes buttons to set the time. One button adds an hour, the other adds a m
 Desamber is a time format created by Devine Lu Linvega. More information about Desamber here: https://wiki.xxiivv.com/#clock
 */
 
-//#define DEBUG 1
-
 #include <LiquidCrystal_I2C.h>
 #include <TimeLib.h>
 #include <RBD_Button.h>
@@ -18,6 +16,76 @@ Desamber is a time format created by Devine Lu Linvega. More information about D
 #define ROW_OFFSET_ZERO 4 // Starting offset for text on first row of LCD 
 #define ROW_OFFSET_ONE 5 // Starting offset for text on second row of LCD
 
+
+class desamberTime
+{
+  public:
+  String beat;
+  String pulse;
+  String formatted;
+
+  String getTime(unsigned long hr, unsigned long minu, unsigned long sec, unsigned long mil)
+  {
+    // Calculate Desamber time
+    unsigned long _dTime = ((hr * 3600000) + (minu * 60000) + (sec * 1000) + mil) / 86.4;
+
+    // Format to string with filler zero characters up to six total characters
+    String _dTimeString = addMissingDigits(String(_dTime), 6);
+    
+    beat = _dTimeString.substring(0, 3);
+    pulse = _dTimeString.substring(3, 6);
+    formatted = beat + ":" + pulse;
+
+    return formatted;
+  }
+
+  String addMissingDigits(String value, int digitsRequired)
+  {
+    int missingDigits = digitsRequired - value.length();
+    String result = "";
+    
+    for (int i = 0; i < missingDigits; i++)
+    {
+      result += "0";
+    }
+    
+    result += value;
+    return result;
+  }
+};
+
+
+class militaryTime
+{
+  public:
+  String formatted;
+
+  String getTime(unsigned long hr, unsigned long minu, unsigned long sec, unsigned long mil)
+  {
+    String formatted = addMissingDigits(String(hr), 2);
+    formatted += ':';
+    formatted += addMissingDigits(String(minu), 2);
+    formatted += ':';
+    formatted += addMissingDigits(String(sec), 2);
+    return formatted;
+  }
+
+  String addMissingDigits(String value, int digitsRequired)
+  {
+    int missingDigits = digitsRequired - value.length();
+    String result = "";
+    
+    for (int i = 0; i < missingDigits; i++)
+    {
+      result += "0";
+    }
+    
+    result += value;
+    return result;
+  }
+};
+
+
 // LCD
 LiquidCrystal_I2C lcd(0x20,16,2);  // Set the LCD address to 0x20 for a 16 chars and 2 line display
 
@@ -25,11 +93,13 @@ LiquidCrystal_I2C lcd(0x20,16,2);  // Set the LCD address to 0x20 for a 16 chars
 RBD::Button buttonHour(BUTTON_PIN_HOUR); // Button for time adjust add hour
 RBD::Button buttonMinute(BUTTON_PIN_MINUTE); // Button for time adjust add minute
 
-// SHARED VARIABLES
+// TIME
 unsigned long _hour = 0;
 unsigned long _minute = 0;
 unsigned long _second = 0;
 unsigned long _milli = 0;
+militaryTime _mTime = militaryTime();
+desamberTime _dTime = desamberTime();
 
 void setup()
 {
@@ -38,10 +108,6 @@ void setup()
   lcd.backlight();
   lcd.home();
   lcd.clear();
-
-  #ifdef DEBUG
-    Serial.begin(9600);
-  #endif
 }
 
 void loop()
@@ -51,7 +117,7 @@ void loop()
   _minute = (unsigned long) minute();
   _second = (unsigned long) second();
 
-  // This keeps our _milli value under 1000 for dTime calculations.
+  // This keeps the _milli value under 1000 for dTime calculations.
   String milliString = String(millis());
   _milli = String(millis()).substring(milliString.length()-3, milliString.length()).toInt();
 
@@ -66,10 +132,10 @@ void loop()
   }
 
   // Print 24-hour time to LCD
-  writeToScreen(getMilitaryTime(), ROW_OFFSET_ZERO, 0);
+  writeToScreen(_mTime.getTime(_hour, _minute, _second, _milli), ROW_OFFSET_ZERO, 0);
 
   // Print Desamber time to LCD
-  writeToScreen(getDesamberTime(), ROW_OFFSET_ONE, 1);
+  writeToScreen(_dTime.getTime(_hour, _minute, _second, _milli), ROW_OFFSET_ONE, 1);
 
   // '...the shortest pulse equals to 8.64 milliseconds...'
   delay(86);
@@ -80,69 +146,3 @@ void writeToScreen(String value, int posX, int posY)
   lcd.setCursor(posX, posY);
   lcd.print(value);
 }
-
-String getMilitaryTime()
-{
-  String milTime = addMissingDigits(String(_hour), 2);
-  milTime += ':';
-  milTime += addMissingDigits(String(_minute), 2);
-  milTime += ':';
-  milTime += addMissingDigits(String(_second), 2);
-  return milTime;
-}
-
-String getDesamberTime()
-{
-  // Calculate Desamber time
-  unsigned long _dTime = ((_hour * 3600000) + (_minute * 60000) + (_second * 1000) + _milli) / 86.4;
-
-  // Format to string with filler zero characters up to six total characters
-  String _dTimeString = addMissingDigits(String(_dTime), 6);
-
-  #ifdef DEBUG
-    Serial.print(addMissingDigits(String(_hour), 2));
-    Serial.print(":");
-    Serial.print(addMissingDigits(String(_minute), 2));
-    Serial.print(":");
-    Serial.print(addMissingDigits(String(_second), 2));
-    Serial.print(":");
-    Serial.print(addMissingDigits(String(_milli), 3));
-    
-    Serial.print("      _dTimeString = ");
-    Serial.print(_dTimeString);
-  
-    //Serial.print("      _dTimeMid = ");
-    //Serial.print(_dTimeMid);
-  
-    Serial.print("      millis() = ");
-    Serial.print(millis());
-   
-    Serial.println();
-  #endif 
-  
-  // Add ':' seperator between beat and pulse 
-  return insertString(_dTimeString, ":", 3);
-}
-
-String addMissingDigits(String value, int digitsRequired)
-{
-  int missingDigits = digitsRequired - value.length();
-  String result = "";
-  
-  for (int i = 0; i < missingDigits; i++)
-  {
-    result += "0";
-  }
-  
-  result += value;
-  return result;
-}
-
-String insertString(String text, String addition, int indexPos)
-{
-  String result = text.substring(0, indexPos);
-  result += addition;
-  result += text.substring(indexPos, text.length());
-  return result;
-}
-
